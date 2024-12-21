fbab2e17c5a40bab0c61a51b400feba75e93d4c1
doukn0wdaway <anisimov.sasha20@gmail.com>
2024-12-21 22:15:14 +0200

    fixes with cursor

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,17 +9,32 @@ import { ProjectSlides } from './slides/projects';
 
 function App() {
   const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const [isMouse, setIsMouse] = useState<boolean>(false);
 
   const handleScroll = () => {
     const position = window.scrollY;
     setScrollPosition(position);
   };
 
+  const handleMouse = () => {
+    if (!isMouse) {
+      setIsMouse(true);
+      const cursor = document.getElementsByClassName(
+        'ipad-cursor'
+      )?.[0] as HTMLElement;
+      if (cursor) {
+        cursor.classList.add('show');
+      }
+    }
+  };
+
   useEffect(() => {
     window.addEventListener('scroll', handleScroll, { passive: true });
+    window.addEventListener('mousemove', handleMouse);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('mousemove', handleMouse);
     };
   }, []);
 
